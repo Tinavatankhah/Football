8e5ecd9dd400115b82e81c83f664fa5919bba41d
dazome <zohabap@gmail.com>
2024-01-08 13:31:46 +0330

    adding hand cursor

--- src/FootballGUI.java
+++ src/FootballGUI.java
@@ -1,4 +1,4 @@
-import java.awt.Color;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
@@ -22,24 +22,31 @@ public class FootballGUI extends JFrame{
 	private void addGuiComponents() {
 		JButton spainBtn=new JButton(loadImage("src/assets/la-liga-logo-.jpg"));
 		spainBtn.setBounds(20, 10,160, 100);
+		spainBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		add(spainBtn);
 		JButton englandBtn=new JButton(loadImage("src/assets/premierleague.png"));
 		englandBtn.setBounds(210, 10,160, 100);
+		englandBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		add(englandBtn);
 		JButton italyBtn=new JButton(loadImage("src/assets/logo-serie-a.jpg"));
 		italyBtn.setBounds(400, 10,160, 100);
+		italyBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		add(italyBtn);
 		JButton iranBtn=new JButton(loadImage("src/assets/iran.jpg"));
 		iranBtn.setBounds(20, 130,160, 100);
+		iranBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		add(iranBtn);
 		JButton germanyBtn=new JButton(loadImage("src/assets/bundesliga-logo-1024x683.png"));
 		germanyBtn.setBounds(210,130,160, 100);
+		germanyBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		add(germanyBtn);
 		JButton franceBtn=new JButton(loadImage("src/assets/franceLigue1.jpg"));
 		franceBtn.setBounds(400, 130,160, 100);
+		franceBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		add(franceBtn);
 		JButton worldCupBtn=new JButton(loadImage("src/assets/worldCup1.png"));
 		worldCupBtn.setBounds(90, 260,400, 100);
+		worldCupBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		add(worldCupBtn);
 		
 	}
