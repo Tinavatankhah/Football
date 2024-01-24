d4be66810c4a2e661cb427b64bcc430ed65ffb8f
unknown <tinavatankhah9@gmail.com>
2024-01-24 22:38:26 +0330

    Added initial functionality for the buttons

--- src/FootballAPI.java
+++ src/FootballAPI.java
@@ -3,44 +3,117 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 public class FootballAPI {
+	String urlString;
+	private String choice;
 
-    String ApiKey ="f52db1114b2d80e1e563c0402e7980c3";
-    private static HttpURLConnection fetchApiResponse(String urlString){
-        try{
-            URL url= new URL(urlString);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("Get");
-            conn.setRequestProperty("x-rapidapi-key","f52db1114b2d80e1e563c0402e7980c3");
-            conn.setRequestProperty("x-rapidapi-host", "v3.football.api-sports.io");
-            conn.connect();
-            return conn;
-        } catch (IOException e){
-        e.printStackTrace();
-    }
-        return null;
+	public JSONObject getFootballData()
+	{
+		 String choice = null;
+		JSONArray leagueData = getLeagueData(choice);
 
+
+	
+		try {
+			HttpURLConnection conn= fetchApiResponse(urlString);
+			if(conn.getResponseCode()!=200)
+			{
+				 System.out.println("Error!! : Could not connect to API");
+				 return null;
+			}
+			else
+			{
+				
+				
+				
+				
+					
+			}
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return null;
 }
-    public static void main(String[] args) {
-String urlString= "https://v3.football.api-sports.io/leagues";
-    HttpURLConnection conn = fetchApiResponse(urlString);
-    if(conn != null)
 
-    {
+	public JSONArray getLeagueData(String choice) {
+		String urlString="";
+	
+		switch(choice)
+		{
+		case "spainBtn": 
+		urlString= "https://v3.football.api-sports.io/leagues";
+			 
+	    break;
+		case "englandBtn": 
+			urlString= "https://v3.football.api-sports.io/leagues";
+			 
+	    break;
+		case "italyBtn": 
+			  urlString= "https://v3.football.api-sports.io/leagues";
+			 
+	    break;
+		case "iranBtn": 
+			 urlString = "https://v3.football.api-sports.io/leagues";
+			 
+	    break;
+		case "germanyBtn": 
+			 urlString =  "https://v3.football.api-sports.io/leagues";
+	    break;
+		case "franceBtn": 
+			 urlString =  "https://v3.football.api-sports.io/leagues";
+			 
+	    break;
+		case "worldCupBtn": 
+			 urlString =  "https://v3.football.api-sports.io/leagues";
+	    break;
+	    default:
+	    	System.out.println("error!!!");
+	    	break;
+		}
+	
+		 HttpURLConnection conn = fetchApiResponse(urlString);
+		    if(conn != null)
 
-        try {
+		    {
 
-            if (conn.getResponseCode() != 200) {
-                System.out.println("Error: could not connect to API");
-            }
-            StringBuilder resultJson = new StringBuilder();
-            Scanner scanner = new Scanner(conn.getInputStream());
-            while (scanner.hasNext()) {
-                resultJson.append(scanner.nextLine());
-            }
-            scanner.close();
-            conn.disconnect();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }}}
+		        try {
+
+		            if (conn.getResponseCode() != 200) {
+		                System.out.println("Error: could not connect to API");
+		            }
+		            StringBuilder resultJson = new StringBuilder();
+		            Scanner scanner = new Scanner(conn.getInputStream());
+		            while (scanner.hasNext()) {
+		                resultJson.append(scanner.nextLine());
+		            }
+		            scanner.close();
+		            conn.disconnect();
+		        } catch (IOException e) {
+		            e.printStackTrace();
+		        }
+		       
+		    }
+			return null;
+			
+	}
+	 private static HttpURLConnection fetchApiResponse(String urlString){
+	        try{
+	            URL url= new URL(urlString);
+	            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	            conn.setRequestMethod("GET");
+	            conn.setRequestProperty("x-rapidapi-key","f52db1114b2d80e1e563c0402e7980c3");
+	            conn.setRequestProperty("x-rapidapi-host", "v3.football.api-sports.io");
+	            conn.connect();
+	            return conn;
+	        } catch (IOException e){
+	        e.printStackTrace();
+	    }
+	        return null;
+
+	}
+}
--- src/FootballGUI.java
+++ src/FootballGUI.java
@@ -1,11 +1,23 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
 import javax.imageio.ImageIO;
-import javax.swing.*; 
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
-public class FootballGUI extends JFrame{
+import org.json.simple.JSONArray; 
+
+public class FootballGUI extends JFrame implements ActionListener {
+	FootballAPI footballAPI=new FootballAPI();
+	JLabel jLabel;
+	
+	   private JTable footballTable;
+	    private DefaultTableModel tableModel;
+
+	
 	
 	FootballGUI()
 	{
@@ -20,36 +32,59 @@ public class FootballGUI extends JFrame{
 	}
 
 	private void addGuiComponents() {
-		JButton spainBtn=new JButton(loadImage("src/assets/la-liga-logo-.jpg"));
-
+		
+		JButton spainBtn=new JButton("spainBtn",loadImage("src/assets/la-liga-logo-.jpg"));
 		spainBtn.setBounds(20, 10,160, 100);
 		spainBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		add(spainBtn);
-		JButton englandBtn=new JButton(loadImage("src/assets/premierleague.png"));
+		spainBtn.addActionListener(this);
+		
+		JButton englandBtn=new JButton("englandBtn",loadImage("src/assets/premierleague.png"));
 		englandBtn.setBounds(210, 10,160, 100);
 		englandBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		add(englandBtn);
-		JButton italyBtn=new JButton(loadImage("src/assets/logo-serie-a.jpg"));
+		englandBtn.addActionListener(this);
+		
+		JButton italyBtn=new JButton("italyBtn",loadImage("src/assets/logo-serie-a.jpg"));
 		italyBtn.setBounds(400, 10,160, 100);
 		italyBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		add(italyBtn);
-		JButton iranBtn=new JButton(loadImage("src/assets/iran.jpg"));
+		italyBtn.addActionListener(this);
+		
+		JButton iranBtn=new JButton("iranBtn",loadImage("src/assets/iran.jpg"));
 		iranBtn.setBounds(20, 130,160, 100);
 		iranBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		add(iranBtn);
-		JButton germanyBtn=new JButton(loadImage("src/assets/bundesliga-logo-1024x683.png"));
+		iranBtn.addActionListener(this);
+		
+		JButton germanyBtn=new JButton("germanyBtn",loadImage("src/assets/bundesliga-logo-1024x683.png"));
 		germanyBtn.setBounds(210,130,160, 100);
 		germanyBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		add(germanyBtn);
-		JButton franceBtn=new JButton(loadImage("src/assets/franceLigue1.jpg"));
+		germanyBtn.addActionListener(this);
+		
+		JButton franceBtn=new JButton("franceBtn",loadImage("src/assets/franceLigue1.jpg"));
 		franceBtn.setBounds(400, 130,160, 100);
 		franceBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		add(franceBtn);
-		JButton worldCupBtn=new JButton(loadImage("src/assets/worldCup1.png"));
+		franceBtn.addActionListener(this);
+		
+		JButton worldCupBtn=new JButton("worldCupBtn",loadImage("src/assets/worldCup1.png"));
 		worldCupBtn.setBounds(90, 260,400, 100);
 		worldCupBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		add(worldCupBtn);
+		worldCupBtn.addActionListener(this);
 		
+	
+      
+		
+	
+		jLabel=new JLabel("salam");
+		jLabel.setBounds(0, 200, 1000, 800);
+		jLabel.setFont(new Font("Dialog" , Font.PLAIN, 18));
+		jLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		jLabel.setForeground(Color.RED);
+		add(jLabel);
 	}
 
 	private Icon loadImage(String resourcePath) {
@@ -62,6 +97,16 @@ public class FootballGUI extends JFrame{
 		}
 		System.out.println("Could not find resource file");
 		return null;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String choice = e.getActionCommand().toString();
+		
+		
+	        jLabel.setText("Computer: " +  footballAPI.getLeagueData(choice));
+	
+		
 	}
 	
 }
